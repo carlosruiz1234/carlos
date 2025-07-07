4a64a7c084e856183bc4889b4b89207177ca1628
carlosruiz1234 <chivitaaaa2008@gmail.com>
2025-07-07 13:06:08 -0500

    Update FizzBuzz.py

--- 5 ejercicios whastapp/FizzBuzz.py
+++ 5 ejercicios whastapp/FizzBuzz.py
@@ -1,10 +1,10 @@
-
-for numero in range  (1,101):
-    if numero % 3 == 0:
-        print("fizz")
-    if numero %5 == 0:
-        print ("buzz")
-    else:
-        numero %3 == 0  and numero %5 == 0
-        print("fizzBuzz")
-    print(numero)
+for numero in range(1, 101):
+    if numero > 50:
+        if numero % 3 == 0 and numero % 5 == 0:
+            print("fizzBuzz")
+        elif numero % 3 == 0:
+            print("fizz")
+        elif numero % 5 == 0:
+            print("buzz")
+        else:
+            print(numero)
